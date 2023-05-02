5fbce6ddba48bd5176b59c0762b72b182c74bdf9
hrkz <hugo.frezat@gmail.com>
2023-05-02 13:16:23 +0200

    Add news content

--- src/data/News.jsx
+++ src/data/News.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import { Link } from "react-router-dom"
 
-import * as Icon from "@heroicons/react/20/solid"
+import { DocsIcon, CodeIcon } from '../Base'
 
 const newsContent = [
+  {
+    id: 4,
+    type: 'acad',
+    title: 'Started postdoc at Institut de Physique du Globe de Paris (IPGP)',
+    date: 'March 1st, 2023',
+    res: [
+    ]
+  },
   {
     id: 3,
     type: 'acad',
     title: 'Obtained Ph.D. from University Grenoble-Alpes',
     date: 'December 9th, 2022',
-    content: <><q>Learning sub-grid dynamics in idealized turbulent systems</q>.</>,
     res: [
       ['docs', 'Thesis', '#']
     ]
@@ -19,7 +26,6 @@ const newsContent = [
     type: 'spub',
     title: <>New paper accepted in <i>Journal of Advances in Modeling Earth Systems</i></>,
     date: 'September  27th, 2022',
-    content: <>Our paper <q><i>A posteriori</i> learning for quasi-geostrophic turbulence parametrization</q> has been accepted for publication.</>,
     res: [
       ['docs', 'Preprint', 'https://arxiv.org/pdf/2204.03911'],
       ['code', 'Code', '#']
@@ -30,7 +36,6 @@ const newsContent = [
     type: 'spub',
     title: <>New paper accepted in <i>Physical Review Fluids</i></>,
     date: 'February  1st, 2021',
-    content: <>Our paper <q>Physical invariance in neural networks for subgrid-scale scalar flux modeling</q> has been accepted for publication.</>,
     res: [
       ['docs', 'Preprint', 'https://arxiv.org/pdf/2010.04663'],
       ['code', 'Code', '#']
@@ -41,17 +46,16 @@ const newsContent = [
     type: 'acad',
     title: 'Started Ph.D. program at University Grenoble-Alpes',
     date: 'October 1st, 2019',
-    content: <><q>Subgrid modeling using machine learning in fluid dynamics</q> with G. Balarac — LEGI, J. Le Sommer — IGE and R. Fablet — IMT Atlantique.</>,
-    res: []
+    res: [
+      ['docs', 'Proposal', '#']
+    ]
   }
 ]
 
-function PostIcon({ type, className }) {
+function NewsIcon({ type, className }) {
   const iconTypeMap = {
-    'acad': <Icon.AcademicCapIcon className={className} />,
-    'spub': <Icon.BookOpenIcon className={className} />,
-    'docs': <Icon.DocumentArrowDownIcon className={className} />,
-    'code': <Icon.CodeBracketSquareIcon className={className} />
+    'docs': <DocsIcon className={className} />,
+    'code': <CodeIcon className={className} />
   }
 
   return (
@@ -62,67 +66,28 @@ function PostIcon({ type, className }) {
 export default function News() {
   return (
     <article>
-      <ol className="relative mx-8 my-8 border-l border-gray-400 dark:border-gray-300">
+      <h4>News</h4>
+      <hr />
       {newsContent.map((news) => (
-        <li key={news.id} className="mb-10 ml-6">
-          <span className="
-            absolute flex 
-            items-center justify-center 
-            w-6 h-6
-            rounded-full
-            -left-3
-            bg-blue-100 dark:bg-blue-900 
-            ring-8 ring-white dark:ring-gray-800 "
-          >
-            <PostIcon type={news.type} className="w-4 h-4 text-blue-800 dark:text-blue-300" />
-          </span>
-          <h1 className="mb-1 text-lg uppercase font-semibold">{news.title}</h1>
-          <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{news.date}</time>
-          <p className="mb-4 text-sm font-normal">{news.content}</p>
-          {news.res.map(([type, title, url]) => (
-            <a 
-              key={title}
-              href={url}
-              className="
-                inline-flex items-center focus:z-10 focus:outline-none
-                px-4 py-2 mr-2 
-                text-sm font-medium hover:text-blue-700 dark:hover:text-white focus:text-blue-700
-                rounded-lg border border-gray-200 dark:border-gray-600 
-                hover:bg-gray-100 dark:hover:bg-gray-700
-                focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 "
-            >
-              <PostIcon type={type} className="w-4 h-4 mr-2" />
-              {title}
+      <div key={news.id} className="news-entry">
+        <h4 className="info">{news.date}</h4>
+        <div className="news-content">
+          <h4 className="title">
+            {news.title}
+          </h4>
+          {news.res.map(([type, desc, url]) => (
+            <a key={desc} href={url} className="resource">
+              {desc}
+              <NewsIcon type={type} className="icon" />
             </a>
           ))}
-        </li>
+        </div>
+      </div>
       ))}
-      </ol>
-      <ul className="flex justify-center my-12">
-        <li>
-          <Link 
-            to="/" 
-            className="px-3 py-2 uppercase text-xs hover:text-blue-700 dark:hover:text-white"
-          >
-            Prev
-          </Link>
-        </li>
-        <li>
-          <Link 
-            to="/" 
-            className="px-3 py-2 font-bold text-xs text-gray-900 dark:text-white border-b-2 border-gray-900 dark:border-white"
-          >
-            1
-          </Link>
-        </li>
-        <li>
-          <Link 
-            to="/" 
-            className="px-3 py-2 uppercase text-xs hover:text-blue-700 dark:hover:text-white"
-          >
-            Next
-          </Link>
-        </li>
+      <ul className="pagination">
+        <li><Link to="/">Prev</Link></li>
+        <li><Link to="/">1</Link></li>
+        <li><Link to="/">Next</Link></li>
       </ul>
     </article>
   )
